6906c0cf16016e6f708c1d2d07ee3908dcf1f8c2
Rithik Raj Pandey <rrpandey8055@gmail.com>
2023-01-06 18:56:49 +0530

    Added routes to different paths

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React,{ useState } from 'react';
 // import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   const [mode, setMode] = useState('light');
   const toggleMode = ()=>{
@@ -16,15 +22,27 @@ function App() {
         setMode('light');
         document.body.style.backgroundColor = 'white';
       }
+      setInterval(() => {
+        document.title =  'TextUtil is am amazing website';
+      }, 1500);
   }
   return (
     <>
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     {/* < Alert alert="This is a Alert" /> */}
     <div className="container my-3">
-    <TextForm heading="Enter the text to analyze" mode={mode} toggleMode={toggleMode}/>
+    <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+          <TextForm heading="Enter the text to analyze" mode={mode} toggleMode={toggleMode}/>
+          </Route>
+        </Switch>
     {/* <About/> */}
     </div>
+    </Router>
     </>
   );
 }
